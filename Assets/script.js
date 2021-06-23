5c0c90a8448c1588f3b68892e60847d0dff451db
hkidd <harrisonakidd@gmail.com>
2021-06-23 17:12:51 -0500

    Adjusted file locations and trying to figure out how to loop between the true types of characters.

--- Assets/script.js
+++ Assets/script.js
@@ -13,7 +13,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// All available characters and numbers
+// Available characters and numbers
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '<', '>', '?', '/'];
@@ -21,6 +21,7 @@ var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // Main generator function
 function generatePassword() {
+
   var charLength = parseInt (prompt("How many total characters would you like? Min = 8, Max = 128", 8));
   console.log(charLength)
 
@@ -35,32 +36,53 @@ function generatePassword() {
 
   // Write if statements to check what needs to be included in password
   // ex if upper = true, grab random uppercase character
+
+  // For uppercase
   var upper = confirm("Would you like uppercase characters?");
     if (upper === true) {
       passwordArr = upperCase
     } 
+    // For lowercase
   var lower = confirm("Would you like lowercase characters?");
     if (lower === true && passwordArr.length > 0) {
       passwordArr.concat(lowerCase)
-    } else if (lower) {
+    } else if (lower || passwordArr.length < 1) {
       passwordArr = lowerCase
     }
+    // For special characters
   var special = confirm("Would you like special characters?");
     if (special === true && passwordArr.length > 0) {
       passwordArr.concat(specialChar)
-    } else if (special) {
+    } else if (special || passwordArr.length < 1) {
       passwordArr = specialChar
     } 
+    // For numbers (will include numbers in the array if everything else is false)
   var num = confirm("Would you like numbers?");
     if (num === true && passwordArr.length > 0) {
       passwordArr.concat(number)
-    } else if (num || passwordArr.length == 0) {
+    } else if (num || passwordArr.length === 0) {
       passwordArr = number
       console.log(passwordArr)
     } 
 
-  console.log(upper, lower, special, num);
+    // Which are true?
+    var trueTypes = upper + lower + special + num;
+    console.log(trueTypes);
 
   // Loop through designated array length (charLength.length times), and pass in random characters or numbers depending on what variables are true (confirm statements)
+
+  
+// Random password actually generated
+  var randomPassword = "";
+
+  for (var i = 0; i < charLength; i++) {
+    var index = Math.floor(Math.random() * passwordArr.length)
+    randomPassword += passwordArr[index];
+  }
+  return randomPassword;
+
+
 }
+
+
 
