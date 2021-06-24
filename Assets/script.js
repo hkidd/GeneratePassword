403a277b36ee4e90b068563a084e15a6291119f9
hkidd <harrisonakidd@gmail.com>
2021-06-23 19:20:05 -0500

    Finally figured out how to concat correctly and got it working.

--- Assets/script.js
+++ Assets/script.js
@@ -22,9 +22,11 @@ var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 // Main generator function
 function generatePassword() {
 
+  // User is asked to input a number between 8 and 128.  Strings will be passed in as integers.
   var charLength = parseInt (prompt("How many total characters would you like? Min = 8, Max = 128", 8));
   console.log(charLength)
 
+  // If user does not imput a number between 8 and 128 they will be asked again
   if (isNaN(charLength)) {
       charLength = parseInt (prompt("How many total characters would you like? Min = 8, Max = 128", 8));
   } else if (charLength < 8 || charLength > 128) {
@@ -32,56 +34,56 @@ function generatePassword() {
   } 
 
   // Initialize the password array for characters/numbers to be passed into
-  let passwordArr = [];
+  var passwordArray = [];
 
   // Write if statements to check what needs to be included in password
-  // ex if upper = true, grab random uppercase character
+  // ex if upper = true, add the upperCase array to the passwordArr array
 
   // For uppercase
   var upper = confirm("Would you like uppercase characters?");
     if (upper === true) {
-      passwordArr = upperCase
+      passwordArray = passwordArray.concat(upperCase)
     } 
+    console.log(passwordArray)
+
     // For lowercase
   var lower = confirm("Would you like lowercase characters?");
-    if (lower === true && passwordArr.length > 0) {
-      passwordArr.concat(lowerCase)
-    } else if (lower || passwordArr.length < 1) {
-      passwordArr = lowerCase
+    if (lower === true && passwordArray.length >= 0) {
+      passwordArray = passwordArray.concat(lowerCase)
+    } else if (lower === true && passwordArray.length === 0) {
+      passwordArray += lowerCase
     }
+    console.log(passwordArray)
+
     // For special characters
   var special = confirm("Would you like special characters?");
-    if (special === true && passwordArr.length > 0) {
-      passwordArr.concat(specialChar)
-    } else if (special || passwordArr.length < 1) {
-      passwordArr = specialChar
+    if (special === true && passwordArray.length >= 0) {
+      passwordArray = passwordArray.concat(specialChar)
+    } else if (special === true && passwordArray.length === 0) {
+      passwordArray += specialChar
     } 
+    console.log(passwordArray)
+
     // For numbers (will include numbers in the array if everything else is false)
   var num = confirm("Would you like numbers?");
-    if (num === true && passwordArr.length > 0) {
-      passwordArr.concat(number)
-    } else if (num || passwordArr.length === 0) {
-      passwordArr = number
-      console.log(passwordArr)
+    if (num === true && passwordArray.length >= 0) {
+      passwordArray = passwordArray.concat(number)
+    } else if (num === false && passwordArray.length === 0) {
+      passwordArray += number
     } 
-
-    // Which are true?
-    var trueTypes = upper + lower + special + num;
-    console.log(trueTypes);
+    console.log(passwordArray)
+    
+    
+  // Random password variable actually generated
+  var randomPassword = [];
 
   // Loop through designated array length (charLength.length times), and pass in random characters or numbers depending on what variables are true (confirm statements)
 
-  
-// Random password actually generated
-  var randomPassword = "";
-
   for (var i = 0; i < charLength; i++) {
-    var index = Math.floor(Math.random() * passwordArr.length)
-     += passwordArr[index];
+    var index = Math.floor(Math.random() * passwordArray.length)
+     randomPassword += passwordArray[index];
   }
   return randomPassword;
-
-
 }
 
 
