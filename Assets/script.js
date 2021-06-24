1d8b596394e9f07968fe8dfcf3a0ce86ad0b25b8
hkidd <harrisonakidd@gmail.com>
2021-06-24 17:08:53 -0500

    Slight js edits.  Started working on README.

--- Assets/script.js
+++ Assets/script.js
@@ -15,7 +15,7 @@ generateBtn.addEventListener("click", writePassword);
 // Available characters and numbers
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '<', '>', '?', '/'];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '~', '`', '<', '>', '?', '/', '[', ']'];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // Main generator function
@@ -50,7 +50,7 @@ function generatePassword() {
     
     // For lowercase:
   var lower = confirm("Would you like lowercase characters?");
-    // This concatenates the lowercase letters onto the existing passwordArray, or, onto an empty array
+    // This concatenates the lowercase letters onto the existing passwordArray, or, onto an empty array if true
     if (lower === true && passwordArray.length >= 0) {
       passwordArray = passwordArray.concat(lowerCase)
     } else if (lower === true && passwordArray.length === 0) {
@@ -59,7 +59,7 @@ function generatePassword() {
 
     // For special characters:
   var special = confirm("Would you like special characters?");
-    // This concatenates the special characters onto the existing passwordArray, or, onto an empty array
+    // This concatenates the special characters onto the existing passwordArray, or, onto an empty array if true
     if (special === true && passwordArray.length >= 0) { 
       passwordArray = passwordArray.concat(specialChar)
     } else if (special === true && passwordArray.length === 0) {
@@ -68,14 +68,14 @@ function generatePassword() {
 
     // For numbers: (will include numbers in the array if everything else is false)
   var num = confirm("Would you like numbers?");
-    // This concatenates the numbers onto the existing passwordArray, or, onto the empty passwordArray even if they say no, so that something is given
+    // This concatenates the numbers onto the existing passwordArray if true, or, onto the empty passwordArray even if they say no, so that something is given
     if (num === true && passwordArray.length >= 0) {
       passwordArray = passwordArray.concat(number)
     } else if (num === false && passwordArray.length === 0) {
       passwordArray = passwordArray.concat(number)
     }
 
-  // Random password variable generated
+  // Random password variable/array generated
   var randomPassword = [];
 
   // Loop through designated array length (charLength times), and pass in random characters or numbers depending on what variables are true (confirmed statements)
@@ -83,6 +83,7 @@ function generatePassword() {
     var index = Math.floor(Math.random() * passwordArray.length)
      randomPassword += passwordArray[index];
   }
+  // Finally, return the newly contructed password to be shown on screen
   return randomPassword;
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # GeneratePassword_HW3
-Homework 3 - Generating a random password
+Homework assignment 3 - Generating a random password using JavaScript
+
+Link to the live page:
+
+## Purpose
+The purpose of this activity was to take a small amount of starter code, and create a fully functional password generator.  This generator takes user imputs to see how many characters they would like in the password (between 8 and 128), then if they would like to include lowercase, uppercase, special, and/or special characters.  After the code runs, the user is presented with a randomly generated password.
+
+## Acceptance Criteria
+```
+GIVEN I need a new, secure password
+
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Getting started
+The starter code we were provided with contained the generate button itself, as well as the function that takes the main generatePassword() result and passes that in to the text box for the user to see.
+
+The first step was to create variables for all lowercase, uppercase, numeric, and special characters.
+
+
+## The bulk of the challenge
+After creating all of the character variables, the user is then asked (using a prompt) how many characters they would like to include.  If the user does not enter a number, or does not enter a number between 8 and 128, they are prompted again:
+
+<img src="Assets/images/user_prompts.png" width=70% height=auto />
+
+
+
+Then, using confirm statements, the user chooses what types of characters they would like to include.  Those character arrays are concatenated onto the (initially) empty passwordArray.  Numbers are automatically given if the user declines all options.  Here's an example of the lowercase if else statement:
+
+<img src="Assets/images/lowercase_if_else.png" width=70% height=auto />
+
+
+
+
